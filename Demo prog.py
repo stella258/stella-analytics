c4a4a8808fa52ad605c38946abdbb84cd08e0bfe
stella258 <86552901+stella258@users.noreply.github.com>
2021-07-08 22:59:26 +0530

    Add files via upload

---
+++ Demo prog.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 22:47:49 2021
+
+@author: 91705
+"""
+
+X=[1,2,3,4,5]
+Y=[2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as aman
+aman.plot(X,Y)
